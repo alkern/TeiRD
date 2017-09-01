fca4764519450868de18d184c3c6d48c0073e3f9
Werkelmann <al-kern@web.de>
2017-09-01 16:25:24 +0200

    fixed bug with empty parts in urn, added structure

--- server/src/main/java/main/controller/RenderController.kt
+++ server/src/main/java/main/controller/RenderController.kt
@@ -2,6 +2,7 @@ package main.controller
 
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.net.MalformedURLException
 
@@ -11,21 +12,32 @@ class RenderController {
     private val INVALID_URN = "invalid urn"
 
     @RequestMapping("/render/{urn}")
-    fun render(@PathVariable urn: String): String {
+    fun render(@PathVariable urn: String, @RequestParam(defaultValue = "default") style: String = "default"): String {
         try {
             val urnResouce = Urn.new(urn)
         } catch (e: MalformedURLException) {
-            return INVALID_URN
+            return INVALID_URN + style
         }
 
-        return urn
+        //TODO get xml from cts
+        //TODO transform xml to html
+
+        val stylesheet = loadStylesheet(style)
+
+        //TODO add css-stylesheet to html
+
+        return "$urn $stylesheet" //TODO return rendered html
+    }
+
+    private fun loadStylesheet(style: String): String {
+        return "$style.css" //TODO load external stylesheet
     }
 }
 
 data class Urn(val namespace: String, val work: String, val passage: String = "", val subreference: String = "") {
     companion object {
         fun new(urn: String): Urn {
-            val parts = urn.split(":")
+            val parts = urn.split(":").filter {it != ""}
             if (parts.size !in 4..6) throw MalformedURLException("Invalid URN $urn")
             if (parts[0] != "urn" || parts[1] != "cts") throw MalformedURLException("Invalid URN $urn")
 
