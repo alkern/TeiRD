53774b987b47515165cccb1cc3c43eaaf78f99b6
Werkelmann <al-kern@web.de>
2017-09-01 16:30:09 +0200

    migrated server.java to kotlin

---
+++ server/src/main/java/main/Server.kt
@@ -0,0 +1,12 @@
+package main
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class Server
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Server::class.java, *args)
+}
+
