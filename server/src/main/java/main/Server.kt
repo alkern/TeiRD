d7ea5da7c3bb0fb4e76ec440183dc51533f8b162
Alexander Kern <al-kern@web.de>
2017-09-09 11:45:37 +0200

    made war runnable on tomcat

--- server/src/main/java/main/Server.kt
+++ server/src/main/java/main/Server.kt
@@ -2,9 +2,15 @@ package main
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
 
 @SpringBootApplication
-open class Server
+open class Server: SpringBootServletInitializer() {
+    override protected fun configure(builder: SpringApplicationBuilder?): SpringApplicationBuilder {
+        return builder!!.sources(Server::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Server::class.java, *args)
--- server/src/test/java/main/controller/UrnTest.kt
+++ server/src/test/java/main/controller/UrnTest.kt
@@ -9,41 +9,49 @@ class UrnTest {
     @Test
     fun testValidUrns() {
         val validUrn = Urn.new("urn:cts:ns:test")
+        if (validUrn == null) {
+            fail()
+            return
+        }
         assertEquals("ns", validUrn.namespace)
         assertEquals("test", validUrn.work)
         assertEquals("", validUrn.passage)
-        assertEquals("", validUrn.subreference)
+        assertEquals(null, validUrn.subreference)
 
         val validUrn2 = Urn.new("urn:cts:ns:test:passage")
+        if (validUrn2 == null) {
+            fail()
+            return
+        }
         assertEquals("ns", validUrn2.namespace)
         assertEquals("test", validUrn2.work)
         assertEquals("passage", validUrn2.passage)
-        assertEquals("", validUrn2.subreference)
+        assertEquals(null, validUrn2.subreference)
 
         val validUrn3 = Urn.new("urn:cts:ns:test:passage:sub")
+        if (validUrn3 == null) {
+            fail()
+            return
+        }
         assertEquals("ns", validUrn3.namespace)
         assertEquals("test", validUrn3.work)
         assertEquals("passage", validUrn3.passage)
         assertEquals("sub", validUrn3.subreference)
     }
 
-    @Test(expected = MalformedURLException::class)
     fun testEmptyUrnThrowsException() {
-        Urn.new("")
+        assertEquals(null, Urn.new(""))
     }
 
-    @Test(expected = MalformedURLException::class)
     fun testTooShortUrnThrowsException() {
-        Urn.new("urn:cts")
+        assertEquals(null, Urn.new("urn:cts"))
     }
 
-    @Test(expected = MalformedURLException::class)
     fun testMissingUrnThrowsException() {
-        Urn.new("cts:ns:work:passage")
+        assertEquals(null, Urn.new("cts:ns:work:passage"))
     }
 
-    @Test(expected = MalformedURLException::class)
     fun testMissingctsThrowsException() {
-        Urn.new("urn:work:passage")
+        assertEquals(null, Urn.new("urn:work:passage"))
     }
 }
