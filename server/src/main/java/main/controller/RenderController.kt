e7b9d90ca6892f811c7b5d2c9573ed31c4843c60
Werkelmann <al-kern@web.de>
2017-09-09 09:29:03 +0200

    restructured controller

--- server/src/main/java/main/controller/RenderController.kt
+++ server/src/main/java/main/controller/RenderController.kt
@@ -4,7 +4,13 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.net.MalformedURLException
+import java.io.File
+import java.io.FileInputStream
+import javax.xml.transform.Result
+import javax.xml.transform.Source
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMResult
+import javax.xml.transform.stream.StreamSource
 
 @RestController
 class RenderController {
@@ -13,33 +19,27 @@ class RenderController {
 
     @RequestMapping("/render/{urn}")
     fun render(@PathVariable urn: String, @RequestParam(defaultValue = "default") style: String = "default"): String {
-        try {
-            val urnResouce = Urn.new(urn)
-        } catch (e: MalformedURLException) {
-            return INVALID_URN + style
-        }
+        val urnResource = Urn.new(urn)
+        if (urnResource == null) return INVALID_URN
 
-        //TODO get xml from cts
-        //TODO transform xml to html
+        val loader = FileLoader()
 
-        val stylesheet = loadStylesheet(style)
+        val source = loader.getXmlSource(urnResource)
+        val target = XslTransformator().transform(source)
 
+        val stylesheet = loader.loadStylesheet(style)
         //TODO add css-stylesheet to html
 
         return "$urn $stylesheet" //TODO return rendered html
-    }
-
-    private fun loadStylesheet(style: String): String {
-        return "$style.css" //TODO load external stylesheet
     }
 }
 
 data class Urn(val namespace: String, val work: String, val passage: String = "", val subreference: String = "") {
     companion object {
-        fun new(urn: String): Urn {
-            val parts = urn.split(":").filter {it != ""}
-            if (parts.size !in 4..6) throw MalformedURLException("Invalid URN $urn")
-            if (parts[0] != "urn" || parts[1] != "cts") throw MalformedURLException("Invalid URN $urn")
+        fun new(urn: String): Urn? {
+            val parts = urn.split(":").filter { it != "" }
+            if (parts.size !in 4..6) return null
+            if (parts[0] != "urn" || parts[1] != "cts") return null
 
             val namespace = parts[2]
             val work = parts[3]
@@ -49,4 +49,26 @@ data class Urn(val namespace: String, val work: String, val passage: String = ""
             return Urn(namespace, work, passage, subreference)
         }
     }
+}
+
+class FileLoader {
+    fun getXmlSource(urnResource: Urn): Source {
+        //TODO get xml from cts
+        return StreamSource(FileInputStream(File("out/production/resources/brand.xml")))
+    }
+
+    fun loadStylesheet(style: String): String {
+        return "$style.css" //TODO load external stylesheet
+    }
+}
+
+class XslTransformator {
+    fun transform(xmlSource: Source): Result {
+        //TODO transform xml to html
+        val transformer = TransformerFactory.newInstance().newTransformer()
+        val result = DOMResult()
+        transformer.transform(xmlSource, result)
+        return result
+    }
+
 }
