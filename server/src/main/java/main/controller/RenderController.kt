7cef5b1e3d5e78da2e7e167ce3728fd659cae4b8
Alexander Kern <al-kern@web.de>
2017-09-09 10:46:17 +0200

    getting xml from cts and transforming with xslt from res

--- server/src/main/java/main/controller/RenderController.kt
+++ server/src/main/java/main/controller/RenderController.kt
@@ -1,15 +1,23 @@
 package main.controller
 
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileInputStream
+import java.nio.charset.StandardCharsets
 import javax.xml.transform.Result
 import javax.xml.transform.Source
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMResult
+import javax.xml.transform.stream.StreamResult
 import javax.xml.transform.stream.StreamSource
 
 @RestController
@@ -17,24 +25,25 @@ class RenderController {
 
     private val INVALID_URN = "invalid urn"
 
-    @RequestMapping("/render/{urn}")
-    fun render(@PathVariable urn: String, @RequestParam(defaultValue = "default") style: String = "default"): String {
+    @RequestMapping("/render/{cts}/{urn:.+}")
+    fun render(@PathVariable cts: String, @PathVariable urn: String, @RequestParam(defaultValue = "default") style: String = "default"): String {
         val urnResource = Urn.new(urn)
         if (urnResource == null) return INVALID_URN
 
         val loader = FileLoader()
 
-        val source = loader.getXmlSource(urnResource)
-        val target = XslTransformator().transform(source)
+        val source = loader.getXmlSource(cts, urnResource)
+        val xsltStylesheet = loader.loadTransformationStylesheet()
+        val target = XslTransformator(xsltStylesheet).transform(source)
 
         val stylesheet = loader.loadStylesheet(style)
         //TODO add css-stylesheet to html
 
-        return "$urn $stylesheet" //TODO return rendered html
+        return target
     }
 }
 
-data class Urn(val namespace: String, val work: String, val passage: String = "", val subreference: String = "") {
+data class Urn(val namespace: String, val work: String, val passage: String = "", val subreference: String?) {
     companion object {
         fun new(urn: String): Urn? {
             val parts = urn.split(":").filter { it != "" }
@@ -44,17 +53,27 @@ data class Urn(val namespace: String, val work: String, val passage: String = ""
             val namespace = parts[2]
             val work = parts[3]
             val passage = if (parts.size > 4) parts[4] else ""
-            val subreference = if (parts.size > 5) parts[5] else ""
+            val subreference = if (parts.size > 5) parts[5] else null
 
             return Urn(namespace, work, passage, subreference)
         }
     }
+
+    fun getUrlParams(): String {
+        var result = "?request=GetPassage&urn=urn:cts:$namespace:$work:$passage"
+        if (this.subreference != null) result + ":${this.subreference}"
+        return result
+    }
 }
 
 class FileLoader {
-    fun getXmlSource(urnResource: Urn): Source {
-        //TODO get xml from cts
-        return StreamSource(FileInputStream(File("out/production/resources/brand.xml")))
+    fun getXmlSource(cts: String, urnResource: Urn): Source {
+        val xmlSource = HTTPClient().get(cts, urnResource)
+        return StreamSource(ByteArrayInputStream(xmlSource.toByteArray(StandardCharsets.UTF_8)))
+    }
+
+    fun loadTransformationStylesheet(): StreamSource {
+        return StreamSource(File("out/production/resources/template.xsl"))
     }
 
     fun loadStylesheet(style: String): String {
@@ -62,13 +81,30 @@ class FileLoader {
     }
 }
 
-class XslTransformator {
-    fun transform(xmlSource: Source): Result {
-        //TODO transform xml to html
-        val transformer = TransformerFactory.newInstance().newTransformer()
-        val result = DOMResult()
+class HTTPClient {
+    private val rest: RestTemplate = RestTemplate()
+    private val headers: HttpHeaders = HttpHeaders()
+
+    init {
+        headers.add("Content-Type", "application/xml;charset=UTF-8")
+        headers.add("Accept", "*/*")
+    }
+
+    fun get(cts: String, urnResource: Urn): String {
+        val uri = "http://$cts.informatik.uni-leipzig.de/pbc/cts/${urnResource.getUrlParams()}&&configuration=divs=true_escapepassage=false"
+        val requestEntity = HttpEntity<String>("", headers)
+        val responseEntity = rest.exchange(uri, HttpMethod.GET, requestEntity, String::class.java)
+        return responseEntity.body.toString()
+    }
+}
+
+class XslTransformator(val style: StreamSource) {
+    fun transform(xmlSource: Source): String {
+        val transformer = TransformerFactory.newInstance().newTransformer(style)
+        val stream = ByteArrayOutputStream()
+        val result = StreamResult(stream)
         transformer.transform(xmlSource, result)
-        return result
+        return stream.toString(StandardCharsets.UTF_8.name())
     }
 
 }
