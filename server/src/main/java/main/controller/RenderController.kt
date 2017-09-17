eb363ad8364a5aed7261eb129fbf42a658db8de8
Alexander Kern <al-kern@web.de>
2017-09-17 10:04:17 +0200

    simplified app

--- server/src/main/java/main/controller/RenderController.kt
+++ server/src/main/java/main/controller/RenderController.kt
@@ -25,15 +25,12 @@ class RenderController {
 
     private val INVALID_URN = "invalid urn"
 
-    @RequestMapping("/{cts}/{server}/{urn:.+}")
-    fun render(@PathVariable cts: String, @PathVariable urn: String, @PathVariable server: String,
+    @RequestMapping("/{server}/{urn:.+}")
+    fun render(@PathVariable urn: String, @PathVariable server: String,
                @RequestParam(defaultValue = "default") style: String): String {
-        val urnResource = Urn.new(urn)
-        if (urnResource == null) return INVALID_URN
-
         val loader = FileLoader()
 
-        val source = loader.getXmlSource(cts, server, urnResource)
+        val source = loader.getXmlSource(server, urn)
         val xsltStylesheet = loader.loadTransformationStylesheet()
         val target = XslTransformator(xsltStylesheet).transform(source)
 
@@ -43,38 +40,14 @@ class RenderController {
     }
 }
 
-data class Urn(val namespace: String, val work: String, val passage: String?, val subreference: String?) {
-    companion object {
-        fun new(urn: String): Urn? {
-            val parts = urn.split(":").filter { it != "" }
-            if (parts.size !in 4..6) return null
-            if (parts[0] != "urn" || parts[1] != "cts") return null
-
-            val namespace = parts[2]
-            val work = parts[3]
-            val passage = if (parts.size > 4) parts[4] else null
-            val subreference = if (parts.size > 5) parts[5] else null
-
-            return Urn(namespace, work, passage, subreference)
-        }
-    }
-
-    fun getUrlParams(): String {
-        var result = "?request=GetPassage&urn=urn:cts:$namespace:$work"
-        if (passage != null) result += ":$passage"
-        if (subreference != null) result += ":$subreference"
-        return result
-    }
-}
-
 class FileLoader {
-    fun getXmlSource(cts: String, server: String, urnResource: Urn): Source {
-        val xmlSource = HTTPClient().get(cts, server, urnResource)
+    fun getXmlSource(server: String, urnResource: String): Source {
+        val xmlSource = HTTPClient().get(server, urnResource)
         return StreamSource(ByteArrayInputStream(xmlSource.toByteArray(StandardCharsets.UTF_8)))
     }
 
     fun loadTransformationStylesheet(): StreamSource {
-        return StreamSource(File("tomcat/webapps/view/template.xsl"))
+        return StreamSource(File("tomcat/webapps/renderer/WEB-INF/classes/template.xsl"))
     }
 
     fun loadStylesheet(style: String): String {
@@ -91,8 +64,8 @@ class HTTPClient {
         headers.add("Accept", "*/*")
     }
 
-    fun get(cts: String, server: String, urnResource: Urn): String {
-        val uri = "http://$cts.informatik.uni-leipzig.de/$server/cts/${urnResource.getUrlParams()}&configuration=divs=true_escapepassage=false"
+    fun get(server: String, urnResource: String): String {
+        val uri = "http://cts.informatik.uni-leipzig.de/$server/cts/?request=GetPassage&urn=$urnResource&configuration=divs=true_escapepassage=false"
         println(uri)
         val requestEntity = HttpEntity<String>("", headers)
         val responseEntity = rest.exchange(uri, HttpMethod.GET, requestEntity, String::class.java)
